7184c1a39c9a3591590c29ff02a13f41ba4e253b
Misael Pérez Chamorro <misaelpcgm@gmail.com>
2018-04-16 21:42:17 -0500

    Initial Commit

---
+++ MyTelephone/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyTelephone
+//
+//  Created by Misael Pérez Chamorro on 4/16/18.
+//  Copyright © 2018 Misael Pérez Chamorro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
